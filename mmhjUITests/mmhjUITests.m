8714c8dc6c5784805dac6624a2dd4bcb4ee08903
cheng yin <wyyincheng@163.com>
2016-03-16 16:36:07 +0800

    Initial Commit

---
+++ mmhjUITests/mmhjUITests.m
@@ -0,0 +1,40 @@
+//
+//  mmhjUITests.m
+//  mmhjUITests
+//
+//  Created by cheng yin on 16/3/16.
+//  Copyright © 2016年 cheng yin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mmhjUITests : XCTestCase
+
+@end
+
+@implementation mmhjUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
